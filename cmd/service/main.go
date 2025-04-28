ddd275b3f00953d9c1a67c9e422838a8c8b99a9d
Adrian Grzemski <adrian.grzemski@gmail.com>
2025-04-28 15:24:41 +0200

    Update main application to serve unit registry and add refscaler dependency

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,12 +1,15 @@
 package main
 
 import "github.com/gofiber/fiber/v2"
+import "github.com/grzadr/refscaler/units"
+
+
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app.Get("/units", func(c *fiber.Ctx) error {
+		return c.SendString(units.EmbeddedUnitRegistry.ToJSON())
 	})
 
 	if err := app.Listen(":3000"); err != nil {
