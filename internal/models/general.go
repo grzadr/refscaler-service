a1d801c22f47c0f4030a08c69a6ae1ba41c600b8
Adrian Grzemski <adrian.grzemski@gmail.com>
2025-04-30 12:08:42 +0200

    Fix formatting of HealthResponse struct fields in general.go

--- internal/models/general.go
+++ internal/models/general.go
@@ -2,8 +2,8 @@ package models
 
 type HealthResponse struct {
 	Ready     bool    `json:"ready"`
-	StartTime string   `json:"starttime"`
+	StartTime string  `json:"starttime"`
 	Uptime    float64 `json:"uptime"`
-	Timestamp string   `json:"timestamp"`
+	Timestamp string  `json:"timestamp"`
 	Version   string  `json:"version"`
 }
