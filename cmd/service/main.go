0f46413e1487261dae060ffffad0964b466a8be0
Adrian Grzemski <adrian.grzemski@gmail.com>
2025-05-02 17:21:06 +0200

    Add Swagger documentation files and initialize Swagger in main application

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -4,12 +4,23 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 	"github.com/grzadr/refscaler-service/internal/routes"
 	"github.com/grzadr/refscaler-service/internal/services"
+
+	_ "github.com/grzadr/refscaler-service/cmd/service/docs"
 )
 
 var Version = ""
 
+// @title RefScaler
+// @version 1.0.0
+// @description This is a service for refscaler app
+// @contact.name Adrian Grzemski
+// @contact.email adrian.grzemski@gmail.com
+// @license.name MIT
+// @host localhost:3000
+// @BasePath /
 func main() {
 	// Initialize Fiber app
 	app := fiber.New()
@@ -21,7 +32,7 @@ func main() {
 
 	services.SetupService(Version)
 
-	// app.Get("/swagger/*", swagger.HandlerDefault)
+	app.Get("/swagger/*", swagger.HandlerDefault)
 
 	// Start server
 	log.Println("Starting server on :3000")
